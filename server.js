2074de5d77a0071a19d64563dc0074eee1a7af12
Carlicia Smith <smith.carlicia@gmail.com>
2020-12-03 19:56:39 -0500

    created client side files and connected to the server - works

--- server.js
+++ server.js
@@ -3,13 +3,30 @@ const cat = express();
 
 const PORT = process.env.PORT || 3000;
 
-// const bodyParser = require('body-parser');
-// cat.use(express.static('public'));
 
-// create application/json parser
-// const jsonParser = bodyParser.json()
-// create application/x-www-form-urlencoded parser
-// const urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+// uses the static method to render the static front end files 
+// static files = basic html + css  + vanilla js
+cat.use(express.static('client'));
+
+// uses the expres urlencoded method to tranlaste all data
+cat.use(express.urlencoded({ extended: true }));
+
+// uses the json() - JavaScript Object Notation =formatter
+cat.use(express.json());
+
+// require the  handlebars package
+const thehandlebars =require("express-handlebars");
+
+// middleware that will allow forthe use of the handlebar files 
+
+// using the express engine method to inform the system that handlebars will be used - also infomring the system
+// to use the main file as it's basic layout 
+cat.engine("handlebars", thehandlebars({defaultLayout: "main"}));
+
+// allows for you to set the iew once the engine is started 
+cat.set("view engine", "handlerbars");
+
 // POST /login gets urlencoded bodies
 // cat.get('/', urlencodedParser, function (req, res) {
 //     res.sendFile(path.join(__dirname + "../public/index.html"));
