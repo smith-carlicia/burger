74c26aab6de45f6834e5c93154cdaa67c4a7c19b
Carlicia Smith <smith.carlicia@gmail.com>
2020-12-06 11:30:24 -0500

    made changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "nschs0912",
-  database: "burgers_db"
-});
+var connection;
+
+// mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "nschs0912",
+//   database: "burgers_db"
+// });
+console.log(process.env);
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "nschs0912",
+        database: "burgers_db"
+    })
+}
 
 // Make connection.
 connection.connect(function(err) {
