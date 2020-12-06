133d18150894fe3fdd292cd546140430949b2561
Carlicia Smith <smith.carlicia@gmail.com>
2020-12-06 15:53:58 -0500

    added table to sequelpro

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,11 @@
-INSERT INTO burger_name
-VALUES ('salmon burger');
-
-INSERT INTO burger_name
-VALUES ('impossible burger');
-
-INSERT INTO burger_name
-VALUES ('veggie burger');
-
-INSERT INTO burger_name
-VALUES ('blackbean burger');
-
-INSERT INTO burger_name
-VALUES ('tuna burger');
-
 -- I want to insert data into my burger table - the data will be a burger name and the devoured status
 INSERT INTO burgers (burger_name, devoured)
 -- I want to insert chick pea as the burger name and flase as the devoured status
 VALUES ("Chick Pea Burger", FALSE);
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("salmon burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("impossible burger", FALSE);
